f24fa451594ee25d322c01a56b2d4152a3ecdd46
Vivekkumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-25 10:24:34 +0530

    .

--- webapp/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PlayerControls from './components/PlayerControls.vue'
-
-const app = createApp(App)
-app.component('PlayerControls', PlayerControls)
-app.mount('#app')
